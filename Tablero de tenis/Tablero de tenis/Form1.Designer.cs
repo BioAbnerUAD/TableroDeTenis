242ee97dbfd11d6e314be416a92c5f20dacc18e4
Luis Gerardo Garay Jimenez <l.gerardogarayjimenez@gmail.com>
2018-09-25 16:00:04 -0500

    Version 1
    Tiene el tablero hecho, una clase tablero y 2 funciones (incompletas) para aumentarle puntos a cada lado

---
+++ Tablero de tenis/Tablero de tenis/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Tablero_de_tenis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LabelSet = new System.Windows.Forms.Label();
+            this.LabelJuego = new System.Windows.Forms.Label();
+            this.LabelScore = new System.Windows.Forms.Label();
+            this.LebelA = new System.Windows.Forms.Label();
+            this.LabelB = new System.Windows.Forms.Label();
+            this.LabelSetsA = new System.Windows.Forms.Label();
+            this.LabelSetsB = new System.Windows.Forms.Label();
+            this.LabelJuegosA = new System.Windows.Forms.Label();
+            this.LabelJuegosB = new System.Windows.Forms.Label();
+            this.LabelScoreA = new System.Windows.Forms.Label();
+            this.LabelScoreB = new System.Windows.Forms.Label();
+            this.CheckBoxA = new System.Windows.Forms.CheckBox();
+            this.CheckBoxB = new System.Windows.Forms.CheckBox();
+            this.ButtonPuntoB = new System.Windows.Forms.Button();
+            this.ButtonPuntoA = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LabelSet
+            // 
+            this.LabelSet.AutoSize = true;
+            this.LabelSet.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelSet.Location = new System.Drawing.Point(69, 24);
+            this.LabelSet.Name = "LabelSet";
+            this.LabelSet.Size = new System.Drawing.Size(64, 37);
+            this.LabelSet.TabIndex = 6;
+            this.LabelSet.Text = "Set";
+            this.LabelSet.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // LabelJuego
+            // 
+            this.LabelJuego.AutoSize = true;
+            this.LabelJuego.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelJuego.Location = new System.Drawing.Point(150, 24);
+            this.LabelJuego.Name = "LabelJuego";
+            this.LabelJuego.Size = new System.Drawing.Size(104, 37);
+            this.LabelJuego.TabIndex = 7;
+            this.LabelJuego.Text = "Juego";
+            // 
+            // LabelScore
+            // 
+            this.LabelScore.AutoSize = true;
+            this.LabelScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelScore.Location = new System.Drawing.Point(260, 24);
+            this.LabelScore.Name = "LabelScore";
+            this.LabelScore.Size = new System.Drawing.Size(100, 37);
+            this.LabelScore.TabIndex = 8;
+            this.LabelScore.Text = "Score";
+            // 
+            // LebelA
+            // 
+            this.LebelA.AutoSize = true;
+            this.LebelA.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LebelA.Location = new System.Drawing.Point(12, 74);
+            this.LebelA.Name = "LebelA";
+            this.LebelA.Size = new System.Drawing.Size(39, 37);
+            this.LebelA.TabIndex = 10;
+            this.LebelA.Text = "A";
+            // 
+            // LabelB
+            // 
+            this.LabelB.AutoSize = true;
+            this.LabelB.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelB.Location = new System.Drawing.Point(13, 120);
+            this.LabelB.Name = "LabelB";
+            this.LabelB.Size = new System.Drawing.Size(38, 37);
+            this.LabelB.TabIndex = 11;
+            this.LabelB.Text = "B";
+            this.LabelB.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // LabelSetsA
+            // 
+            this.LabelSetsA.AutoSize = true;
+            this.LabelSetsA.Font = new System.Drawing.Font("Microsoft Tai Le", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelSetsA.Location = new System.Drawing.Point(89, 74);
+            this.LabelSetsA.Name = "LabelSetsA";
+            this.LabelSetsA.Size = new System.Drawing.Size(34, 40);
+            this.LabelSetsA.TabIndex = 12;
+            this.LabelSetsA.Text = "0";
+            // 
+            // LabelSetsB
+            // 
+            this.LabelSetsB.AutoSize = true;
+            this.LabelSetsB.Font = new System.Drawing.Font("Microsoft Tai Le", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelSetsB.Location = new System.Drawing.Point(89, 122);
+            this.LabelSetsB.Name = "LabelSetsB";
+            this.LabelSetsB.Size = new System.Drawing.Size(34, 40);
+            this.LabelSetsB.TabIndex = 13;
+            this.LabelSetsB.Text = "0";
+            // 
+            // LabelJuegosA
+            // 
+            this.LabelJuegosA.AutoSize = true;
+            this.LabelJuegosA.Font = new System.Drawing.Font("Microsoft Tai Le", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelJuegosA.Location = new System.Drawing.Point(181, 74);
+            this.LabelJuegosA.Name = "LabelJuegosA";
+            this.LabelJuegosA.Size = new System.Drawing.Size(34, 40);
+            this.LabelJuegosA.TabIndex = 14;
+            this.LabelJuegosA.Text = "0";
+            // 
+            // LabelJuegosB
+            // 
+            this.LabelJuegosB.AutoSize = true;
+            this.LabelJuegosB.Font = new System.Drawing.Font("Microsoft Tai Le", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelJuegosB.Location = new System.Drawing.Point(181, 120);
+            this.LabelJuegosB.Name = "LabelJuegosB";
+            this.LabelJuegosB.Size = new System.Drawing.Size(34, 40);
+            this.LabelJuegosB.TabIndex = 15;
+            this.LabelJuegosB.Text = "0";
+            // 
+            // LabelScoreA
+            // 
+            this.LabelScoreA.AutoSize = true;
+            this.LabelScoreA.Font = new System.Drawing.Font("Microsoft Tai Le", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelScoreA.Location = new System.Drawing.Point(298, 74);
+            this.LabelScoreA.Name = "LabelScoreA";
+            this.LabelScoreA.Size = new System.Drawing.Size(34, 40);
+            this.LabelScoreA.TabIndex = 18;
+            this.LabelScoreA.Text = "0";
+            // 
+            // LabelScoreB
+            // 
+            this.LabelScoreB.AutoSize = true;
+            this.LabelScoreB.Font = new System.Drawing.Font("Microsoft Tai Le", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelScoreB.Location = new System.Drawing.Point(298, 120);
+            this.LabelScoreB.Name = "LabelScoreB";
+            this.LabelScoreB.Size = new System.Drawing.Size(34, 40);
+            this.LabelScoreB.TabIndex = 19;
+            this.LabelScoreB.Text = "0";
+            // 
+            // CheckBoxA
+            // 
+            this.CheckBoxA.AutoSize = true;
+            this.CheckBoxA.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CheckBoxA.Location = new System.Drawing.Point(267, 86);
+            this.CheckBoxA.Name = "CheckBoxA";
+            this.CheckBoxA.Size = new System.Drawing.Size(15, 14);
+            this.CheckBoxA.TabIndex = 20;
+            this.CheckBoxA.UseVisualStyleBackColor = true;
+            // 
+            // CheckBoxB
+            // 
+            this.CheckBoxB.AutoSize = true;
+            this.CheckBoxB.Location = new System.Drawing.Point(267, 122);
+            this.CheckBoxB.Name = "CheckBoxB";
+            this.CheckBoxB.Size = new System.Drawing.Size(15, 14);
+            this.CheckBoxB.TabIndex = 21;
+            this.CheckBoxB.UseVisualStyleBackColor = true;
+            this.CheckBoxB.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // ButtonPuntoB
+            // 
+            this.ButtonPuntoB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonPuntoB.Location = new System.Drawing.Point(188, 163);
+            this.ButtonPuntoB.Name = "ButtonPuntoB";
+            this.ButtonPuntoB.Size = new System.Drawing.Size(162, 36);
+            this.ButtonPuntoB.TabIndex = 22;
+            this.ButtonPuntoB.Text = "Punto B";
+            this.ButtonPuntoB.UseVisualStyleBackColor = true;
+            this.ButtonPuntoB.Click += new System.EventHandler(this.ButtonPuntoB_Click);
+            // 
+            // ButtonPuntoA
+            // 
+            this.ButtonPuntoA.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonPuntoA.Location = new System.Drawing.Point(20, 163);
+            this.ButtonPuntoA.Name = "ButtonPuntoA";
+            this.ButtonPuntoA.Size = new System.Drawing.Size(162, 36);
+            this.ButtonPuntoA.TabIndex = 23;
+            this.ButtonPuntoA.Text = "Punto A";
+            this.ButtonPuntoA.UseVisualStyleBackColor = true;
+            this.ButtonPuntoA.Click += new System.EventHandler(this.ButtonPuntoA_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 205);
+            this.Controls.Add(this.ButtonPuntoA);
+            this.Controls.Add(this.ButtonPuntoB);
+            this.Controls.Add(this.CheckBoxB);
+            this.Controls.Add(this.CheckBoxA);
+            this.Controls.Add(this.LabelScoreB);
+            this.Controls.Add(this.LabelScoreA);
+            this.Controls.Add(this.LabelJuegosB);
+            this.Controls.Add(this.LabelJuegosA);
+            this.Controls.Add(this.LabelSetsB);
+            this.Controls.Add(this.LabelSetsA);
+            this.Controls.Add(this.LabelB);
+            this.Controls.Add(this.LebelA);
+            this.Controls.Add(this.LabelScore);
+            this.Controls.Add(this.LabelJuego);
+            this.Controls.Add(this.LabelSet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label LabelSet;
+        private System.Windows.Forms.Label LabelJuego;
+        private System.Windows.Forms.Label LabelScore;
+        private System.Windows.Forms.Label LebelA;
+        private System.Windows.Forms.Label LabelB;
+        private System.Windows.Forms.Label LabelSetsA;
+        private System.Windows.Forms.Label LabelSetsB;
+        private System.Windows.Forms.Label LabelJuegosA;
+        private System.Windows.Forms.Label LabelJuegosB;
+        private System.Windows.Forms.Label LabelScoreA;
+        private System.Windows.Forms.Label LabelScoreB;
+        private System.Windows.Forms.CheckBox CheckBoxA;
+        private System.Windows.Forms.CheckBox CheckBoxB;
+        private System.Windows.Forms.Button ButtonPuntoB;
+        private System.Windows.Forms.Button ButtonPuntoA;
+    }
+}
+
---
+++ Tablero de tenis/Tablero de tenis/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tablero_de_tenis
+{
+    public partial class Form1 : Form
+    {
+        Tablero ElTablero;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonPuntoA_Click(object sender, EventArgs e)
+        {
+            if (ElTablero.GamePointA == true)
+            {
+                ElTablero.GamePointA = false;
+                CheckBoxA.Checked = false;
+                ElTablero.ScoreA = 0;
+
+                ElTablero.GamesA += 1;
+                LabelJuegosA.Text = ElTablero.GamesA.ToString();
+            }
+                switch (ElTablero.ScoreA)
+            {
+                case 0:
+                    ElTablero.ScoreA = 15;
+                    LabelScoreA.Text = ElTablero.ScoreA.ToString();
+                    break;
+                case 15:
+                    ElTablero.ScoreA = 30;
+                    LabelScoreA.Text = ElTablero.ScoreA.ToString();
+                    break;
+                case 30:
+                    ElTablero.ScoreA = 40;
+                    LabelScoreA.Text = ElTablero.ScoreA.ToString();
+                    break;
+                case 40:
+                    ElTablero.GamePointA = true;
+                    CheckBoxA.Checked = true;
+                    break;
+            }
+            if (ElTablero.GamesA == 6)
+            {
+                ElTablero.GamesA = 0;
+                LabelJuegosA.Text = ElTablero.GamesA.ToString();
+                ElTablero.SetsA += 1;
+                LabelSetsA.Text = ElTablero.SetsA.ToString();
+            }
+           
+        }
+
+        private void ButtonPuntoB_Click(object sender, EventArgs e)
+        {
+            ElTablero.ScoreB += 15;
+            //Debe ser la misma funcion wue ButtonPuntoA solo hay que cambiar las variables de A por las de B
+        }
+    }
+
+    public class Tablero
+    {
+        public int ScoreA = 0;
+        public int ScoreB = 0;
+        public bool GamePointA = false;
+        public bool GamePointB = false;
+        public int GamesA = 0;
+        public int GamesB = 0;
+        public int SetsA = 0;
+        public int SetsB = 0;
+        //La lista se hace para hacer un juego precargado, bool = true es punto para equipo A, bool = false es punto para equipo B.
+        public List<bool> PartidoA = new List<bool> { true, false, true };
+        public List<bool> PartidoB = new List<bool> { false, true, false };
+    }
+}
